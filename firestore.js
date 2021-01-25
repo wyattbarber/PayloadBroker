4a165c8446d470921ac74aa7b4d3acf9e1bd95ac
wyattbarber <wyattjbarber@gmail.com>
2021-01-24 21:38:42 -0500

    Added ip address set function

--- firestore.js
+++ firestore.js
@@ -19,5 +19,33 @@ module.exports = {
             res.status(404).send({ error: 'Could not locate document for this client' });
             throw 'Error opening document';
         }
+    },
+
+    set: async function(req, res) {
+        const client = req.query.client_id
+            ? req.query.client_id
+            : req.body.client_id;
+        const param = req.query.param
+            ? req.query.param
+            : req.body.param;
+        const value = req.query.value
+            ? req.query.value
+            : req.body.value;
+
+        const clientDoc = await db.collection('Home Data').doc(client).get();
+        if (clientDoc.exists) {
+            try {
+                clientDoc.update({param: value});
+            }
+            catch (e) {
+                res.status(400).send({ error: 'Failed to update data' });
+                return;
+            }
+        }
+        else {
+            res.status(404).send({ error: 'Could not locate document for this client' });
+            throw 'Error opening document';
+        }
+        
     }
 };
--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 const firestore = require('./firestore');
 
+exports.address_ap = (req, res) => {
+    const source_ip = req.ip;
+
+    // Get data to validate source
+    const data = firestore.getUserData(req, res);
+    if (req.query.key != data.HomeSecret) {
+        res.status(400).send({error : 'Failed to verify source identity'});
+        return;
+    }
+
+    // Source verified, store new ip
+    var newReq = req;
+    newReq.body.param = 'HomeAddress';
+    newReq.body.value = source_ip;
+    firestore.set(newReq, res);
+
+    // Respond to request
+    res.status(202).send({ip: source_ip});
+    return;
+}
