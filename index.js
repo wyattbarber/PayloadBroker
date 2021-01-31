74523f8ffb9398775f741e760a4bd15da980bc65
wyattbarber <wyattjbarber@gmail.com>
2021-01-30 20:55:50 -0500

    Using .then() with firestore access

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ const firestore = require('./firestore');
 
 exports.link_home = (req, res) => {
     const secret = req.body.secret;
-    var data = firestore.getUserData(req, res);
-
-    // Verify source of request
-    if (secret != data.HomeSecret) {
-        res.status(400).send({error: "Could not verify source "+secret + ", " + data.HomeSecret});
-        return;
-    }
-    // Source verified, set home url
-    var newReq = req;
-    newReq.body.param = 'HomeAddress';
-    newReq.body.value = req.body.url;
-    firestore.set(req, res);
-    data = firestore.getUserData(req, res);
-
-    // Send response with verification key and set url
-    res.status(202).send({
-        key: data.HomeAccesKey,
-        url: data.HomeAddress
+    firestore.getUserData(req, res).then((data) => {
+        // Verify source of request
+        if (secret != data.HomeSecret) {
+            res.status(400).send({ error: "Could not verify source " + secret + ", " + data.HomeSecret });
+            return;
+        }
+        // Source verified, set home url
+        var newReq = req;
+        newReq.body.param = 'HomeAddress';
+        newReq.body.value = req.body.url;
+        firestore.set(req, res);
+        firestore.getUserData(req, res).then((newData) => {
+            // Send response with verification key and set url
+            res.status(202).send({
+                key: newData.HomeAccesKey,
+                url: newData.HomeAddress
+            });
+        });
     });
 }
